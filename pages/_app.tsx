b3080a9cfefe931ec24ddcda1ad4a17bd79c8720
Rudra Patel <rudrapatel26122004@gmail.com>
2023-02-12 12:24:40 -0500

    added dark styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import {useMemo} from "react";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Box from "@mui/material/Box";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  const darkTheme = useMemo(() => createTheme({
+    palette: {
+      mode: 'dark'
+    }
+  }), [])
+
+  return <ThemeProvider theme={darkTheme}>
+    <Box sx={{height: '100vh', bgcolor: 'background.default'}}>
+      <Component {...pageProps} />
+    </Box>
+  </ThemeProvider>
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 export default function Document() {
   return (
     <Html lang="en">
